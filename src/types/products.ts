c06a99f306c41d32db8d79bfe3bf6ba6bdbae3e2
DESKTOP-IBAEHR8 <soodeh.arvin@gmail.com>
2026-02-14 16:22:23 +0330

    chore: extend Product type with thumbnail and images fields

--- src/types/products.ts
+++ src/types/products.ts
@@ -2,7 +2,9 @@ export interface Product {
   id: number;
   title: string;
   description: string;
-  price: number;
+  price: number;  
+  thumbnail: string;      
+  images: string[];       
 }
 
 export interface ProductsResponse {
